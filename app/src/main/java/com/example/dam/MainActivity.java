a59100a3df30e3c9e866c85a46b0529eadf8e323
David <david.chelu@yahoo.com>
2021-06-24 02:58:16 +0300

    Added functions to detect input.

--- app/src/main/java/com/example/dam/MainActivity.java
+++ app/src/main/java/com/example/dam/MainActivity.java
@@ -6,6 +6,8 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Handler;
+import android.view.MotionEvent;
 import android.view.View;
 
 import android.widget.RelativeLayout;
@@ -18,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView piValueTextView;
 
     private boolean initialized = false;
-    private float centerX, centerY, touchX, touchY;
+    private float centerX, centerY, touchX, touchY, piValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -27,34 +29,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         layout = findViewById(R.id.relativeLayout);
-        // text = findViewById(R.id.text);
         centerButton = findViewById(R.id.centerButton);
         outlineButton = findViewById(R.id.outlineButton);
         piValueTextView = findViewById(R.id.piValueTextView);
 
-        // final Handler h = new Handler();
+        final Handler handler = new Handler();
 
-        // setTitle("Sleep Timer");
-        // layout.setOnTouchListener(handleTouch);
+        setTitle("PI Values");
+        layout.setOnTouchListener(tapHandle);
 
         centerButton.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
+                piValueTextView.setText(Float.toString(centerX));
 //                setTimer(30);
             }
         });
 
-        /*h.postDelayed(new Runnable()
+        handler.postDelayed(new Runnable()
         {
             public void run()
             {
-                updateUI();
+                updateGUI();
 
-                h.postDelayed(this, 50);
+                handler.postDelayed(this, 50);
             }
-        }, 100);*/
+        }, 100);
     }
 
     @Override
@@ -84,5 +86,55 @@ public class MainActivity extends AppCompatActivity {
 
             initialized = true;
         }
+    }
+
+    private void updateGUI()
+    {
+        new Thread()
+        {
+            public void run()
+            {
+                if (layout.isFocused())
+                {
+                    try
+                    {
+                        runOnUiThread(() -> {
+                            moveOutlineButton();
+                            piValueTextView.setText(Float.toString(touchX));
+                        });
+                        Thread.sleep(100);
+                    }
+                    catch (Exception ex)
+                    {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        }.start();
+    }
+
+    private View.OnTouchListener tapHandle = new View.OnTouchListener()
+    {
+        @Override
+        public boolean onTouch(View v, MotionEvent e)
+        {
+            switch (e.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    touchX = e.getX();
+                    touchY = e.getY();
+                    break;
+                case MotionEvent.ACTION_UP:
+                    break;
+            }
+
+            return false;
+        }
+    };
+
+    private void moveOutlineButton()
+    {
+
     }
 }
